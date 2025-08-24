c4321e2c07e8b721e6a1687335bd0db06ef514eb
Lucas <lucasalvesshakur@gmail.com>
2025-08-24 11:47:19 -0300

    Update settings.py

--- workout_api/configs/settings.py
+++ workout_api/configs/settings.py
@@ -1,8 +1,14 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
-    DB_URL: str = Field(default='postgresql+asyncpg://workout:workout@localhost/workout')
+    DB_URL: str = Field(
+        default="postgresql+asyncpg://workout:workout@localhost/workout"
+    )
+
+    class Config:
+        env_file = ".env"   # permite carregar variáveis do arquivo .env
 
 
 settings = Settings()
