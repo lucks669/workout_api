bcd6add776209d7a1f5c90b67a0f0ffb02ce2340
Lucas <lucasalvesshakur@gmail.com>
2025-08-24 12:25:02 -0300

    Update settings.py

--- workout_api/configs/settings.py
+++ workout_api/configs/settings.py
@@ -2,7 +2,7 @@ from pydantic import Field
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DB_URL: str = Field(default='postgresql+asyncpg://workout:workout@localhost/workout')
+    DB_URL: str = Field(default="postgresql+asyncpg://workout:workout@localhost:5432/workout")
 
 
 settings = Settings()
